856e7f2b5a1a04666b174174c036fe17175fb553
Ekat22003 <ekatorubarova22@gmail.com>
2025-12-09 22:23:55 +0500

    Update task_1293.java

--- timus/task_1293.java
+++ timus/task_1293.java
@@ -9,12 +9,6 @@ public class task_1293 {
         int A = in.nextInt();
         int B = in.nextInt();
 
-        if (N < 1 || N > 100 || A < 1 || A > 100 || B < 1 || B > 100) {
-            System.out.println("Ошибка: числа должны быть в диапазоне от 1 до 100");
-            in.close();
-            return;
-        }
-
         int S = N * A * B; // с одной стороны
         int S2 = S * 2; // с обеих сторон
 
@@ -22,3 +16,4 @@ public class task_1293 {
         in.close();
     }
 }
+
